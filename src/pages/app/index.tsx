cf6a14bf4b3b886a15b6aa7286fb578764683e8c
Illia Ilin <ilin.i.1337@gmail.com>
2024-03-04 17:26:53 +0100

    enhancement/APP-Implement-auth-flow
    - Implement get self test flow
    - Refactor according to taken code style

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,10 +1,12 @@
 // outsource dependencies
+import toast from 'react-hot-toast';
 import { memo, useCallback } from 'react';
 
 // assets
 import logo from '@/assets/logo-icon.svg';
 import API from '@/services/api-private.ts';
 import AuthService from '@/services/auth.ts';
+import getErrorMessage from '@/services/errors.ts';
 import { API_NAMES } from '@/services/api-helpers.ts';
 import { useControllerActions } from '@/pages/controller.ts';
 
@@ -21,8 +23,18 @@ const AppLayout = memo(() => {
     AuthService.removeToken();
   }, [update]);
 
+  const handleGetSelf = useCallback(async () => {
+    try {
+      const response = await API({ url: '/auth/me', method: 'GET' });
+      console.log(response);
+    } catch (error) {
+      toast.error(getErrorMessage(error));
+    }
+  }, []);
+
   return <div className="flex w-full h-full items-center justify-center min-h-screen flex-col gap-y-4">
     <img src={logo} alt="" width={300} height={300} className="animate-pulse"/>
+    <button type="button" onClick={handleGetSelf} className="btn-primary">Get Self</button>
     <button className="btn-secondary" type="button" onClick={logout}>Logout</button>
   </div>;
 });
